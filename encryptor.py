a07157390e36bc182e5a30186a09f837dc0c2a62
rush1dan <rushidan10@gmail.com>
2022-08-15 17:35:33 +0600

    Python upgraded to 3.10.6

--- encryptor.py
+++ encryptor.py
@@ -1,8 +1,7 @@
-from turtle import bye
 from cryptography.fernet import Fernet
 
 
-def encrypt(msg: str, cipher: Fernet, printKey=False) -> bytes:
+def encrypt(msg: str | bytes, cipher: Fernet, printKey=False) -> bytes:
     byteText = bytes(msg, 'utf-8')
     cipherText = cipher.encrypt(byteText)
 
