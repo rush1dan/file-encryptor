9141b438eeb8ed914ba9aeaddabd0bdedb91f6a8
rush1dan <rushidan10@gmail.com>
2022-08-20 01:17:27 +0600

    Frame test bg colors removed

--- pages.py
+++ pages.py
@@ -24,27 +24,27 @@ class Options_Page(Page):
         self.columnconfigure(index=0, minsize=int(self.page_width))
 
         frm_label = tk.Frame(master=self, relief=tk.FLAT,
-                             borderwidth=0, bg="red")
+                             borderwidth=0)
         frm_label.grid(row=0, column=0, sticky="nsew")
         lbl_options = tk.Label(
             master=frm_label, text="Encrypt or Decrypt?", font=("Arial", 15))
         lbl_options.place(relx=0.5, rely=0.5, anchor=tk.CENTER)
 
         frm_btn = tk.Frame(master=self, relief=tk.FLAT,
-                           borderwidth=0, bg="green")
+                           borderwidth=0)
         frm_btn.grid(row=1, column=0, sticky="nsew")
         frm_btn.rowconfigure(0, minsize=int(self.page_height/2))
         frm_btn.columnconfigure([0, 1], minsize=int(self.page_width/2))
 
         frm_btn_encrypt = tk.Frame(
-            master=frm_btn, relief=tk.RAISED, borderwidth=0, bg="yellow")
+            master=frm_btn, relief=tk.RAISED, borderwidth=0)
         frm_btn_encrypt.grid(row=0, column=0)
         btn_encrypt = tk.Button(master=frm_btn_encrypt,
                                 text="Encrypt", command=None, font=("Arial", 15), border=6, borderwidth=6)
         btn_encrypt.pack()
 
         frm_btn_decrypt = tk.Frame(
-            master=frm_btn, relief=tk.RAISED, borderwidth=0, bg="cyan")
+            master=frm_btn, relief=tk.RAISED, borderwidth=0)
         frm_btn_decrypt.grid(row=0, column=1)
         btn_decrypt = tk.Button(master=frm_btn_decrypt,
                                 text="Decrypt", command=None, font=("Arial", 15), border=6, borderwidth=6)
