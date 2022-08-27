301ac8828579f58d011a3eea2f869bef5dfdbbec
rush1dan <rushidan10@gmail.com>
2022-08-28 00:48:45 +0600

    Pyinstaller instructions added

--- main.py
+++ main.py
@@ -16,3 +16,8 @@ if __name__ == "__main__":
         run_from_right_click(sys.argv[1])
     except:
         run_as_standalone_application()
+
+
+# ***To Installer as .exe run command in the following way:***
+# pyinstaller --onefile --paths C:\PythonProjects\FileEnDecryptor\.venv\Lib\site-packages main.py
+# and rename the generated main.exe to SimpleFileEncryptor.exe
