4da82ef2e55e9858dbed47332ca7be9eddf2a99a
rush1dan <rushidan10@gmail.com>
2022-09-10 11:20:31 +0600

    Operation mode argument added to setup

--- setup.py
+++ setup.py
@@ -1,5 +1,4 @@
 from tkinter import messagebox
-from tkinter import filedialog
 import os
 import winreg
 import ctypes
@@ -31,13 +30,13 @@ def register_in_windows_registry(encryption_menu_name="", decryption_menu_name="
             with winreg.OpenKey(hkey, r'*\shell') as subkey:
                 winreg.CreateKey(subkey, encryption_menu_name)
                 with winreg.OpenKey(subkey, encryption_menu_name, 0, winreg.KEY_WRITE) as encryption_menukey:
+                    winreg.SetValueEx(encryption_menukey, "", 0, winreg.REG_SZ, "Encrypt File(s)")
                     winreg.SetValueEx(encryption_menukey, "AppliesTo",
                                       0, winreg.REG_SZ, "NOT System.FileExtension:=.enc")
                     command_name = "command"
                     winreg.CreateKey(encryption_menukey, command_name)
                     with winreg.OpenKey(encryption_menukey, command_name, 0, winreg.KEY_WRITE) as commandkey:
-                        winreg.SetValueEx(
-                            commandkey, "", 0, winreg.REG_SZ, f"\"{exefilepath}\" %1")
+                        winreg.SetValueEx(commandkey, "", 0, winreg.REG_SZ, f"\"{exefilepath}\" --encrypt %1")
 
             # Set Decryptor:
             encrypted_file_extension = ".enc"
@@ -50,26 +49,22 @@ def register_in_windows_registry(encryption_menu_name="", decryption_menu_name="
                 with winreg.OpenKey(encrypted_file_type_key, "shell") as shellkey:
                     winreg.CreateKey(shellkey, decryption_menu_name)
                     with winreg.OpenKey(shellkey, decryption_menu_name) as decryption_menukey:
+                        winreg.SetValueEx(decryption_menukey, "", 0, winreg.REG_SZ, "Decrypt File(s)")
                         command_name = "command"
                         winreg.CreateKey(decryption_menukey, command_name)
                         with winreg.OpenKey(decryption_menukey, command_name, 0, winreg.KEY_WRITE) as commandkey:
-                            winreg.SetValueEx(
-                                commandkey, "", 0, winreg.REG_SZ, f"\"{exefilepath}\" %1")
+                            winreg.SetValueEx(commandkey, "", 0, winreg.REG_SZ, f"\"{exefilepath}\" --decrypt %1")
 
             # Create New Encrypted File Extension in Registry and register newly created Encrypted File Type under it
             # and configure the extension to behave similar to a .txt extension
             winreg.CreateKey(hkey, encrypted_file_extension)
             with winreg.OpenKey(hkey, encrypted_file_extension, 0, winreg.KEY_WRITE) as encrypted_file_extension_key:
-                winreg.SetValueEx(encrypted_file_extension_key,
-                                  "", 0, winreg.REG_SZ, encrypted_file_type)
-                winreg.SetValueEx(encrypted_file_extension_key,
-                                  "Content Type", 0, winreg.REG_SZ, "text/plain")
-                winreg.SetValueEx(encrypted_file_extension_key,
-                                  "PerceivedType", 0, winreg.REG_SZ, "text")
+                winreg.SetValueEx(encrypted_file_extension_key, "", 0, winreg.REG_SZ, encrypted_file_type)
+                winreg.SetValueEx(encrypted_file_extension_key, "Content Type", 0, winreg.REG_SZ, "text/plain")
+                winreg.SetValueEx(encrypted_file_extension_key, "PerceivedType", 0, winreg.REG_SZ, "text")
 
     else:
-        ctypes.windll.shell32.ShellExecuteW(
-            None, "runas", sys.executable, " ".join(sys.argv), None, 1)
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
 
     return None
 
@@ -78,8 +73,7 @@ if __name__ == "__main__":
     app_dir = get_application_exe(os.path.dirname(os.path.realpath(__file__)))
 
     if app_dir != None:
-        register_in_windows_registry(
-            encryption_menu_name="Encrypt", decryption_menu_name="Decrypt", exefilepath=app_dir)
+        register_in_windows_registry(encryption_menu_name="Encrypt", decryption_menu_name="Decrypt", exefilepath=app_dir)
     else:
         messagebox.showerror(title="Application Not Found Error",
                              message="FileEncryptor.exe not found. Did you move it somewhere else?")
