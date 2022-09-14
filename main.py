b70264d775752249baf494722fcc21e2f0cb2114
rush1dan <rushidan10@gmail.com>
2022-09-14 23:17:44 +0600

    Time stamp based initialization file writing safety implemented

--- main.py
+++ main.py
@@ -17,34 +17,69 @@ def run_as_standalone_application():
 if __name__ == "__main__":
     try:
         # Run Single Instance even for multiple files:
-        if not os.path.exists("C:\\PythonProjects\\FileEnDecryptor\\Data\\Initialization.txt"):
-            if not os.path.exists("C:\\PythonProjects\\FileEnDecryptor\\Data"):
-                os.makedirs("C:\\PythonProjects\\FileEnDecryptor\\Data")
+        if not os.path.exists("C:\\PythonProjects\\FileEnDecryptor\\Data"):
+            os.makedirs("C:\\PythonProjects\\FileEnDecryptor\\Data")
+            if not os.path.exists("C:\\PythonProjects\\FileEnDecryptor\\Data\\Initialization.txt"):
                 open("C:\\PythonProjects\\FileEnDecryptor\\Data\\Initialization.txt", 'w').close()
 
         first_instance = False
 
-        with open("C:\\PythonProjects\\FileEnDecryptor\\Data\\Initialization.txt", "r+", buffering=1) as file_init:
-            lines = file_init.readlines()
-            if len(lines) == 0:  # File not created yet, hence no instance running
-                # Write the file path of the file selected
-                file_init.seek(0, 2)
+        file_init = open("C:\\PythonProjects\\FileEnDecryptor\\Data\\Initialization.txt", "r+", buffering=1)
+
+        lines = file_init.readlines()
+
+        if len(lines) == 0:  # After fresh program installation
+            # Write the file path of the file selected
+            file_init.seek(0, 0)
+            file_init.write(str(time.time()) + "\n")
+            file_init.write(sys.argv[2] + "\n")
+            file_init.flush()
+            file_init.close()
+
+            first_instance = True
+
+        else:  # File has been written to atleast once
+            try:
+                time_since_last_write = float(lines[0])
+
+                current_time = time.time()
+                if current_time - time_since_last_write > 1.5:
+                    print("Elapsed time more than single session wait time. Re-initializing file...")
+
+                    file_init.close()
+
+                    file_init = open("C:\\PythonProjects\\FileEnDecryptor\\Data\\Initialization.txt", "w")
+                    file_init.write(str(time.time()) + "\n")
+                    file_init.write(sys.argv[2] + "\n")
+                    file_init.flush()
+                    file_init.close()
+
+                    first_instance = True
+                else:
+                    file_init.seek(0, 2)
+                    file_init.write(sys.argv[2] + "\n")
+                    file_init.flush()
+                    file_init.close()
+            except:
+                print("Time stamp not found. Re-initializing file...")
+                
+                file_init.close()
+
+                file_init = open("C:\\PythonProjects\\FileEnDecryptor\\Data\\Initialization.txt", "w")
+                file_init.write(str(time.time()) + "\n")
                 file_init.write(sys.argv[2] + "\n")
                 file_init.flush()
+                file_init.close()
 
                 first_instance = True
-
-            else:  # File already created, hence one instance running
-                file_init.seek(0, 2)
-                file_init.write(sys.argv[2] + "\n")
-                file_init.flush()
+            
 
         if first_instance:
             #Wait for other files to conclude writing to the initializer:
             time.sleep(1.0)
             #Read all file paths from init file and store in list:
             with open("C:\\PythonProjects\\FileEnDecryptor\\Data\\Initialization.txt", "r") as file_init:
-                selected_files = [line.strip() for line in file_init.readlines()] 
+                selected_files = [line.strip() for line in file_init.readlines()[1:]] 
             
             data.set_data(operation_arg=sys.argv[1], files=selected_files)
 
