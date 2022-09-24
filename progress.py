a6775e50e33b747f23265958b03f42986de57c99
rush1dan <rushidan10@gmail.com>
2022-09-24 12:30:00 +0600

    Label animation working with tkinter window mainloop but cannot inject data from elsewhere

--- progress.py
+++ progress.py
@@ -11,6 +11,18 @@ def center_window(window: tk.Tk, width=300, height=200):
     y = int((screen_height/2)) - int((height/2))
     window.geometry(f'{width}x{height}+{x}+{y}')
 
+def animate_label(window: tk.Tk, label: tk.Label, files_encrypting: int, text: str, dots: str):
+    #text.format(files_encrypto)
+    dots += "."
+    if len(dots) > 3:
+        dots = ""
+    label.config(text=text + dots)
+    window.after(1000, lambda: animate_label(window, label, 1, text, dots))
+
+def increase_file():
+    global files_encrypto
+    files_encrypto = 1
+
 def show_progress():
     progress_window = tk.Tk()
     progress_window.title("Progress")
@@ -26,23 +38,19 @@ def show_progress():
     progress_window.resizable(0, 0)
 
     center_window(progress_window, width=windowWidth, height=windowHeight)
-    
-    files_encrypted = 1
+
     dots = ""
-    text = "Encrypting Files {files_encrypted}/3".format(files_encrypted=files_encrypted)
+    text = "Encrypting Files {}/3".format(1)
 
     label = tk.Label(master=progress_window, text=text, font=("Arial", 15))
     label.pack()
 
-    while True:
-        time.sleep(1)
-        dots += "."
-        if len(dots) > 3:
-            dots = ""
-        label.config(text=text + dots)
-        progress_window.update()
-    #progress_window.mainloop()
+    button = tk.Button(master=progress_window, text="Click", command=increase_file)
+    button.pack()
 
+    animate_label(window=progress_window, label=label, files_encrypting=-1, text=text, dots=dots)
+
+    progress_window.mainloop()
 
 show_progress()
 
