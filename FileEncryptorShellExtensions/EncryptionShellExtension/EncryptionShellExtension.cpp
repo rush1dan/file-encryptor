2194c4b1fd6fd3b633293bbe31b6bb24c7a23770
rush1dan <rushidan10@gmail.com>
2022-10-25 20:10:04 +0600

    DllUnregisterServer() implemented

--- FileEncryptorShellExtensions/EncryptionShellExtension/EncryptionShellExtension.cpp
+++ FileEncryptorShellExtensions/EncryptionShellExtension/EncryptionShellExtension.cpp
@@ -90,7 +90,7 @@ HRESULT __stdcall DllRegisterServer()
 
 
 	//Create Handler Key:
-	lpSubKey = L"SOFTWARE\\Classes\\*\\Shellex\\ContextMenuHandlers\\" + DLL_REG_NAME;
+	lpSubKey = L"SOFTWARE\\Classes\\*\\shellex\\ContextMenuHandlers\\" + DLL_REG_NAME;
 	result = RegCreateKeyEx(HKEY_LOCAL_MACHINE, lpSubKey.c_str(), 0, NULL, REG_OPTION_NON_VOLATILE, KEY_WRITE, NULL, &hkey, &lpDisp);
 	if (result != ERROR_SUCCESS) { return E_UNEXPECTED; }
 
@@ -115,5 +115,51 @@ HRESULT __stdcall DllRegisterServer()
 
 HRESULT __stdcall DllUnregisterServer()
 {
-	return E_NOTIMPL;
+	HKEY hkey;
+
+	//Check if InprocServer32 key exists and if it does delete it
+	std::wstring lpSubKey = L"SOFTWARE\\Classes\\CLSID\\" + WStringFromCLSID(CLSID_EncryptionShlExt) + L"\\InprocServer32";
+	LONG result = RegOpenKeyEx(HKEY_LOCAL_MACHINE, lpSubKey.c_str(), 0, KEY_ALL_ACCESS, &hkey);
+	if (result == ERROR_SUCCESS)
+	{
+		result = RegDeleteKey(HKEY_LOCAL_MACHINE, lpSubKey.c_str());
+		if (result != ERROR_SUCCESS) { return E_UNEXPECTED; }
+	}
+
+	//Check if GUID key exists and if it does delete it
+	lpSubKey = L"SOFTWARE\\Classes\\CLSID\\" + WStringFromCLSID(CLSID_EncryptionShlExt);
+	result = RegOpenKeyEx(HKEY_LOCAL_MACHINE, lpSubKey.c_str(), 0, KEY_ALL_ACCESS, &hkey);
+	if (result == ERROR_SUCCESS)
+	{
+		result = RegDeleteKey(HKEY_LOCAL_MACHINE, lpSubKey.c_str());
+		if (result != ERROR_SUCCESS) { return E_UNEXPECTED; }
+	}
+	RegCloseKey(hkey);
+
+	//Delete Value from approved list:
+	lpSubKey = L"SOFTWARE\\Microsoft\\Windows\\CurrentVersion\\Shell Extensions\\Approved";
+	result = RegOpenKeyEx(HKEY_LOCAL_MACHINE, lpSubKey.c_str(), 0, KEY_ALL_ACCESS, &hkey);
+	if (result == ERROR_SUCCESS)
+	{
+		result = RegDeleteValue(hkey, WStringFromCLSID(CLSID_EncryptionShlExt).c_str());
+		if (result != ERROR_SUCCESS) { return E_UNEXPECTED; }
+	}
+	RegCloseKey(hkey);
+
+
+	//Delete handler key:
+	lpSubKey = L"SOFTWARE\\Classes\\*\\shellex\\ContextMenuHandlers\\" + DLL_REG_NAME;
+	result = RegOpenKeyEx(HKEY_LOCAL_MACHINE, lpSubKey.c_str(), 0, KEY_ALL_ACCESS, &hkey);
+	if (result == ERROR_SUCCESS)
+	{
+		result = RegDeleteKey(HKEY_LOCAL_MACHINE, lpSubKey.c_str());
+		if (result != ERROR_SUCCESS) { return E_UNEXPECTED; }
+	}
+	RegCloseKey(hkey);
+
+
+	//Alert that there has been a change:
+	SHChangeNotify(SHCNE_ASSOCCHANGED, SHCNF_IDLIST, NULL, NULL);
+
+	return S_OK;
 }
