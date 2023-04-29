632776e94870df5b5a925e28c72f9a9742f018b5
rush1dan <rushidan10@gmail.com>
2023-04-29 18:32:35 +0600

    Error message code simplified

--- FileEncryptorShellExtensions/DecryptionShellExtension/customutils.cpp
+++ FileEncryptorShellExtensions/DecryptionShellExtension/customutils.cpp
@@ -1,4 +1,5 @@
 #include "customutils.h"
+#include <system_error>
 
 std::wstring GetModuleFileDirectory(HINSTANCE hinstDLL)
 {
@@ -17,18 +18,7 @@ std::string GetLastErrorAsString()
         return std::string(); //No error message has been recorded
     }
 
-    LPSTR messageBuffer = nullptr;
-
-    //Ask Win32 to give us the string version of that message ID.
-    //The parameters we pass in, tell Win32 to create the buffer that holds the message for us (because we don't yet know how long the message string will be).
-    size_t size = FormatMessageA(FORMAT_MESSAGE_ALLOCATE_BUFFER | FORMAT_MESSAGE_FROM_SYSTEM | FORMAT_MESSAGE_IGNORE_INSERTS,
-        NULL, errorMessageID, MAKELANGID(LANG_NEUTRAL, SUBLANG_DEFAULT), (LPSTR)&messageBuffer, 0, NULL);
-
-    //Copy the error message into a std::string.
-    std::string message(messageBuffer, size);
-
-    //Free the Win32's string's buffer.
-    LocalFree(messageBuffer);
+    std::string message = std::system_category().message(errorMessageID);
 
     return message;
 }
